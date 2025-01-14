c41227965539b338f4d5cb27ac47969a045fc5a8
vedansh-tyagi245 <vedanshtyagi245@gmail.com>
2025-01-14 14:41:22 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 // Define the Hello component that will display when navigating to /hello
 function Hello() {
@@ -13,7 +13,8 @@ function App() {
         <h1>Welcome to My App</h1>
         <nav>
           <ul>
-            <li><a href="/hello">Hello Page</a></li>
+            {/* Use Link instead of <a> for React Router compatibility */}
+            <li><Link to="/hello">Hello Page</Link></li>
           </ul>
         </nav>
 
@@ -23,7 +24,7 @@ function App() {
           <Route path="/" element={<h2>Welcome to the Home Page!</h2>} />
           
           {/* Hello Route */}
-          <Route path="/hello" element={<div>Welcome to hello page</div>} />
+          <Route path="/hello" element={<Hello />} />
         </Routes>
       </div>
     </Router>
